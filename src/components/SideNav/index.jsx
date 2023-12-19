b4b7485ae232f5f48e97526c0aea627dd65e9a50
Abhishek4298 <abhishekbhavsar143@gmail.com>
2023-12-19 15:13:56 +0530

    Completed Side Navigation Panel.

--- src/components/SideNav/index.jsx
+++ src/components/SideNav/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Button from "../Button";
 import SideNavChildren from "./SideNavChildren";
 
 const SideNav = () => {
@@ -15,7 +14,7 @@ const SideNav = () => {
         className={`fixed inset-0 bg-black opacity-50 z-20 transition-opacity duration-300 ${
           isNavOpen ? "block" : "hidden"
         }`}
-        onClick={toggleNav} 
+        onClick={toggleNav}
       ></div>
 
       <SideNavChildren isOpen={isNavOpen}>
@@ -27,11 +26,24 @@ const SideNav = () => {
                 className="text-lg text-white hover:text-blue-300 focus:text-blue-300 px-4 py-2 flex items-center transition-colors duration-150 ease-in-out"
               >
                 <svg
-                  /* SVG icon for button */ className="w-6 h-6 mr-2"
+                 className="w-6 h-6 mr-2"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                 >
-                  {/* SVG path here */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
                 </svg>
                 Button
               </a>
@@ -42,28 +54,36 @@ const SideNav = () => {
                 className="text-lg text-white hover:text-blue-300 focus:text-blue-300 px-4 py-2 flex items-center transition-colors duration-150 ease-in-out"
               >
                 <svg
-                  /* SVG icon for modal */ className="w-6 h-6 mr-2"
+                  className="w-6 h-6 mr-2"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                 >
-                  {/* SVG path here */}
                 </svg>
                 Modal
               </a>
             </li>
-            {/* Add additional menu items here */}
           </ul>
         </nav>
       </SideNavChildren>
 
-      {/* Button that stays fixed and is visible */}
       <div className="fixed top-5 left-5 z-30">
-        <Button
+        <div
           onClick={toggleNav}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="cursor-pointer"
+          // className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
-          {isNavOpen ? "Close" : "Menu"}
-        </Button>
+          {isNavOpen ? (
+            <h2 className="z-5 text-white">👈</h2>
+          ) : (
+            <div>
+              <svg viewBox="0 0 100 80" width="40" height="40">
+                <rect width="100" height="20"></rect>
+                <rect y="30" width="100" height="20"></rect>
+                <rect y="60" width="100" height="20"></rect>
+              </svg>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
