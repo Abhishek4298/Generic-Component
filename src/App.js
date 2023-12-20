2001bae9187e66305290746382f61254f8019fc4
Abhishek4298 <abhishekbhavsar143@gmail.com>
2023-12-20 11:56:39 +0530

    Added custom routing and preview more sample Button component.

--- src/App.js
+++ src/App.js
@@ -1,71 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Button from "./components/Button";
 import SideNav from "./components/SideNav";
-import Modal from "./components/Modal";
+import CustomRoute from "./CustomRoute";
 
 const App = () => {
-  const handleClick = () => {
-    alert("Button clicked!");
-  };
-  const handleClickTwo = (data) => {
-    alert(`Button clicked with data: ${data}`);
-  };
-
   return (
     <div className="h-screen flex justify-center items-center">
-      <Router>
-        <Routes>
-          <Route
-            path="/button"
-            element={
-              <div className="flex">
-                <Button
-                  color="red"
-                  borderColor="purple"
-                  rounded="md"
-                  size="lg"
-                  onClick={handleClick}
-                >
-                  Basic Button
-                </Button>
-                <Button
-                  color="blue"
-                  borderColor="blue"
-                  rounded="md"
-                  icon={
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M5 13l4 4L19 7"
-                      />
-                    </svg>
-                  }
-                  iconPosition="left"
-                  onClick={() => handleClickTwo("GHEEEE")}
-                >
-                  Button with Icon
-                </Button>
-              </div>
-            }
-          />
-        </Routes>
-        <Routes>
-          <Route path="/modal" element={<Modal color="red" header='Modal header' content="Modal Body"/>} />
-        </Routes>
-
-        <div>
-          <SideNav />
-        </div>
-      </Router>
+      <CustomRoute />
+      <SideNav />
     </div>
   );
 };
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,4 +1,3 @@
-// Button.jsx
 import React from "react";
 import useIcon from "../../hooks/useIcon";
 
@@ -22,19 +21,18 @@ const Button = ({
 
   const baseClasses =
     "inline-flex items-center justify-center px-4 py-2 font-semibold focus:outline-none";
-
-  const colorClasses = color
+  const colorClasses = color.length
     ? `text-black bg-${color}-500 hover:bg-${color}-600`
     : "text-white bg-blue-500 hover:bg-blue-600";
-  const borderClasses = borderColor ? `border border-${borderColor}-500` : "";
-  const roundedClasses = rounded ? `rounded-${rounded}` : "rounded";
+  const borderClasses = borderColor?.length ? `border border-${borderColor}-500` : "";
+  const roundedClasses = rounded?.length ? `rounded-${rounded}` : "rounded";
   const sizeClass = size ? `text-${size}` : "text-md";
   const classes = `${baseClasses} ${colorClasses} ${borderClasses} ${roundedClasses} ${sizeClass}`;
 
   return (
     <button className={classes} {...props} onClick={handleClick}>
       {iconPosition === "left" && iconElement}
-      <span className="flex-grow">{children}</span>{" "}
+      <span className="flex-grow">{children}</span>
       {iconPosition === "right" && iconElement}
     </button>
   );
