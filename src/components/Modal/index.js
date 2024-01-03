7abe86c52af3ae3a189ec5fe07b4218880f89d5f
Manohar Reddy Aleti <manohar.aleti@Excelra.com>
2024-01-03 18:45:42 +0530

    adde more content

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,21 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { closeIcon } from "../../constant";
 import { tw } from "@twind/react";
-const Modal = ({ isOpen, onClose, children, showCloseIcon, header, content, size, position, customCloseIcon,showBorder,borderColor,borderWidth }) => {
+const Modal = ({ isOpen, onClose, children, showCloseIcon, header, content, position, customCloseIcon, showBorder, borderColor, borderWidth, modalHeight, modalWidth }) => {
   const modalClasses = isOpen
     ? `fixed inset-0 flex z-50 ${getPositionClass(position)}`
     : 'hidden';
 
-  const modalSizeClasses = {
-    small: 'w-64',
-    medium: 'w-96',
-    large: 'w-2/3',
-    auto: 'w-auto',
-  };
-
-  const sizeClass = modalSizeClasses[size] || modalSizeClasses.medium;
-
-  function getPositionClass(position)  {
+  function getPositionClass(position) {
     switch (position) {
       case 'left':
         return 'justify-start items-center';
@@ -33,7 +24,7 @@ const Modal = ({ isOpen, onClose, children, showCloseIcon, header, content, size
 
   return (
     <div className={`${modalClasses} bg-black bg-opacity-50`}>
-      <div className={tw`${showBorder ? `border border-${borderWidth} border-${borderColor}-900`:'border-none'} bg-white p-4 shadow-md ${sizeClass} ${getPositionClass(position)}`}>
+      <div className={tw`${showBorder ? `border border-${borderWidth} border-${borderColor}-900` : 'border-none'} bg-white p-4 shadow-md ${getPositionClass(position)} max-w-full max-h-full overflow-x-auto overflow-y-auto`} style={{ height: modalHeight, width: modalWidth }}>
         {showCloseIcon && (
           <button onClick={onClose} className="float-right right-2 top-2 text-gray-600 hover:text-gray-800">
             {customCloseIcon ? (
@@ -58,18 +49,21 @@ Modal.propTypes = {
   showCloseIcon: PropTypes.bool,
   header: PropTypes.string,
   content: PropTypes.node,
-  size: PropTypes.oneOf(['small', 'medium', 'large', 'auto']),
   position: PropTypes.oneOf(['left', 'right', 'top', 'bottom']),
   customCloseIcon: PropTypes.node,
   showBorder: PropTypes.bool,
   borderColor: PropTypes.string,
-  borderWidth: PropTypes.string
+  borderWidth: PropTypes.string,
+  modalHeight: PropTypes.string,
+  modalWidth: PropTypes.string,
 };
 Modal.defaultProps = {
   showCloseIcon: true,
-  size: 'auto',
   showBorder: false,
   borderColor: 'black',
-  borderWidth: '2'
+  borderWidth: '2',
+  modalHeight: 'auto', 
+  modalWidth: 'auto',  
 };
 export default Modal;
+
