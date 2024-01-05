113f772e4cdb2fa5fd7d4d2183b95422f71e710e
Abhishek4298 <abhishekbhavsar143@gmail.com>
2024-01-05 16:07:25 +0530

    Remove unnecessary code.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import SideNav from "./components/SideNav";
 import CustomRoute from "./CustomRoute";
-// import Home from "./components/MenuBar/Home";
 
 const App = () => {
   return (
     <div className="h-screen">
       <CustomRoute />
       <SideNav />
-      {/* <Home/> */}
     </div>
   );
 };
